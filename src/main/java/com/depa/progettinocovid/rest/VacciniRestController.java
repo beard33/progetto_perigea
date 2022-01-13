cc5e27e7d991c963ccb7a8173b643c1e25cd185b
Alessandro <depasquale.a@tuta.io>
2022-01-13 14:10:52 +0100

    response più succinte

--- src/main/java/com/depa/progettinocovid/rest/VacciniRestController.java
+++ src/main/java/com/depa/progettinocovid/rest/VacciniRestController.java
@@ -1,7 +1,7 @@
 package com.depa.progettinocovid.rest;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,122 +19,110 @@ public class VacciniRestController {
 	
 	// Restituire il numero totale della regione lombardia dei vaccinati con singola dose
 	@GetMapping(path = "tot_singola")
-	public ResponseEntity<Response<AggregationResults<Object>>> totSingola () {
-		AggregationResults<Object> body = aggregator.somma("dose1");
+	public ResponseEntity<Response<Document>> totSingola () {
+		Document body = aggregator.somma("dose1");
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Totale una dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Totale una dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Restituire il numero totale della regione lombardia dei vaccinati con doppia dose 
 	@GetMapping(path = "tot_doppia")
-	public ResponseEntity<Response<AggregationResults<Object>>> totDoppia () {
-		AggregationResults<Object> body = aggregator.somma("dose2");
+	public ResponseEntity<Response<Document>> totDoppia () {
+		Document body = aggregator.somma("dose2");
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Totale due dosi", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Totale due dosi", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Restituire il numero totale della regione lombardia dei vaccinati con singola dose per provincia
 	@GetMapping(path = "tot_singola/prov")
-	public ResponseEntity<Response<AggregationResults<Object>>> totSingolaProvincia (@RequestParam String sigla) {		
-		AggregationResults<Object> body = aggregator.sommaFiltra("dose1", sigla);
+	public ResponseEntity<Response<Document>> totSingolaProvincia (@RequestParam String sigla) {		
+		Document body = aggregator.sommaFiltra("dose1", sigla);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Totale singola dose " + sigla , body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Totale singola dose " + sigla , body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Restituire il numero totale della regione lombardia dei vaccinati con doppia dose per provincia
 	@GetMapping(path = "tot_doppia/prov")
-	public ResponseEntity<Response<AggregationResults<Object>>> totDoppiaProvincia (@RequestParam String sigla) {
-		AggregationResults<Object> body = aggregator.sommaFiltra("dose2", sigla);
+	public ResponseEntity<Response<Document>> totDoppiaProvincia (@RequestParam String sigla) {
+		Document body = aggregator.sommaFiltra("dose2", sigla);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Totale doppia dose " + sigla, body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Totale doppia dose " + sigla, body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Lista ordinata per numero di vaccinati in doppia dose di tutta la lombardia
 	@GetMapping(path = "lista_doppia")
-	public ResponseEntity<Response<AggregationResults<Object>>> listaDoppia () {
-		AggregationResults<Object> body = aggregator.listaDoppia();
+	public ResponseEntity<Response<Document>> listaDoppia () {
+		Document body = aggregator.listaDoppia();
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Vaccinati doppia dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Vaccinati doppia dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Lista ordinata per numero di vaccinati in doppia dose di una data provincia
 	@GetMapping(path = "lista_doppia/prov")
-	public ResponseEntity<Response<AggregationResults<Object>>> listaDoppiaProvincia (@RequestParam String sigla) {
-		AggregationResults<Object> body = aggregator.listaDoppiaFiltra(sigla);
+	public ResponseEntity<Response<Document>> listaDoppiaProvincia (@RequestParam String sigla) {
+		Document body = aggregator.listaDoppiaFiltra(sigla);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Vaccinati doppia dose " + sigla, body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Vaccinati doppia dose " + sigla, body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Trovare il comune con più vaccinati in singola dose
 	@GetMapping(path = "comune_max_singola")
-	public ResponseEntity<Response<AggregationResults<Object>>> comuneMaxSingola () {
-		AggregationResults<Object> body = aggregator.capoSomma("dose1", Capo.max);
+	public ResponseEntity<Response<Document>> comuneMaxSingola () {
+		Document body = aggregator.capoSomma("dose1", Capo.max);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Comune con più vaccinati una dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Comune con più vaccinati una dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Trovare il comune con più vaccinati in doppia dose
 	@GetMapping(path = "comune_max_doppia")
-	public ResponseEntity<Response<AggregationResults<Object>>> comuneMaxDoppia () {
-		AggregationResults<Object> body = aggregator.capoSomma("dose2", Capo.max);
+	public ResponseEntity<Response<Document>> comuneMaxDoppia () {
+		Document body = aggregator.capoSomma("dose2", Capo.max);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Comune con più vaccinati doppia dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Comune con più vaccinati doppia dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Trovare il comune con meno vaccinati in singola dose
 	@GetMapping(path = "comune_min_singola")
-	public ResponseEntity<Response<AggregationResults<Object>>> comuneMinSingola () {
-		AggregationResults<Object> body = aggregator.capoSomma("dose1", Capo.min);
+	public ResponseEntity<Response<Document>> comuneMinSingola () {
+		Document body = aggregator.capoSomma("dose1", Capo.min);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Comune con meno vaccinati singola dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);	
+		Response<Document> res = successResponse("Comune con meno vaccinati singola dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);	
 	}
 	
 	// Trovare il comune con meno vaccinati in doppia dose
 	@GetMapping(path = "comune_min_doppia")
-	public ResponseEntity<Response<AggregationResults<Object>>> comuneMinDoppia () {
-		AggregationResults<Object> body = aggregator.capoSomma("dose2", Capo.min);
+	public ResponseEntity<Response<Document>> comuneMinDoppia () {
+		Document body = aggregator.capoSomma("dose2", Capo.min);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Comune con più vaccinati doppia dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Comune con più vaccinati doppia dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Trovare il comune con più vaccinati in singola dose per una data provincia
 	@GetMapping(path = "comune_max_singola/prov")
-	public ResponseEntity<Response<AggregationResults<Object>>> comuneMaxSingola (@RequestParam String sigla) {
-		AggregationResults<Object> body = aggregator.capoSommaFiltra("dose1", Capo.max, sigla);
+	public ResponseEntity<Response<Document>> comuneMaxSingola (@RequestParam String sigla) {
+		Document body = aggregator.capoSommaFiltra("dose1", Capo.max, sigla);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Comune in provincia " + sigla + "con più vaccinati una dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Comune in provincia " + sigla + "con più vaccinati una dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	// Trovare il comune con più vaccinati in doppia dose per una data provincia
 	@GetMapping(path = "comune_max_doppia/prov")
-	public ResponseEntity<Response<AggregationResults<Object>>> comuneMaxDoppia (@RequestParam String sigla) {
-		AggregationResults<Object> body = aggregator.capoSommaFiltra("dose2", Capo.max, sigla);
+	public ResponseEntity<Response<Document>> comuneMaxDoppia (@RequestParam String sigla) {
+		Document body = aggregator.capoSommaFiltra("dose2", Capo.max, sigla);
 		
-		Response<AggregationResults<Object>> res = successResponse(
-				"Comune in provincia " + sigla + "con più vaccinati doppia dose", body);
-		return new ResponseEntity<>(res, HttpStatus.OK);
+		Response<Document> res = successResponse("Comune in provincia " + sigla + "con più vaccinati doppia dose", body);
+		return new ResponseEntity<Response<Document>>(res, HttpStatus.OK);
 	}
 	
 	private <T> Response<T> successResponse(String desc, T body) {
--- src/main/java/com/depa/progettinocovid/service/ConteggioAggregationService.java
+++ src/main/java/com/depa/progettinocovid/service/ConteggioAggregationService.java
@@ -1,11 +1,11 @@
 package com.depa.progettinocovid.service;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
@@ -17,41 +17,41 @@ public class ConteggioAggregationService {
 	
 	public enum Capo {min, max}
 	
-	public AggregationResults<Object> somma(String field) {
-		return template.aggregate(Aggregation.newAggregation(Aggregation.group().sum(field).as(field + "_tot")), "conteggio", Object.class);
+	public Document somma(String field) {
+		return template.aggregate(Aggregation.newAggregation(Aggregation.group().sum(field).as(field + "_tot")), "conteggio", Object.class).getRawResults();
 	}
 	
-	public AggregationResults<Object> sommaFiltra(String field, String sigla) {
+	public Document sommaFiltra(String field, String sigla) {
 		Aggregation aggregation = Aggregation.newAggregation(
 				Aggregation.match(Criteria.where("sigla").is(sigla)),
 				Aggregation.group().sum(field).as(field + "_tot"));
-		return template.aggregate(aggregation, "conteggio", Object.class);
+		return template.aggregate(aggregation, "conteggio", Object.class).getRawResults();
 	}
 	
-	public AggregationResults<Object> capoSomma(String field, Capo capo) {
+	public Document capoSomma(String field, Capo capo) {
 		Aggregation aggregation = Aggregation.newAggregation(
 				Aggregation.sort(Sort.by(capo == Capo.max ? Direction.DESC : Direction.ASC, field)),
 				Aggregation.limit(1));
-		return template.aggregate(aggregation, "conteggio", Object.class);
+		return template.aggregate(aggregation, "conteggio", Object.class).getRawResults();
 	}
 	
-	public AggregationResults<Object> capoSommaFiltra(String field, Capo capo, String sigla) {
+	public Document capoSommaFiltra(String field, Capo capo, String sigla) {
 		Aggregation aggregation = Aggregation.newAggregation(
 				Aggregation.match(Criteria.where("sigla").is(sigla)),
 				Aggregation.sort(Sort.by(capo == Capo.max ? Direction.DESC : Direction.ASC, field)),
 				Aggregation.limit(1));
-		return template.aggregate(aggregation, "conteggio", Object.class);
+		return template.aggregate(aggregation, "conteggio", Object.class).getRawResults();
 	}
 	
-	public AggregationResults<Object> listaDoppia () {
+	public Document listaDoppia () {
 		Aggregation aggregation = Aggregation.newAggregation(Aggregation.sort(Sort.by(Direction.DESC, "dose2")));
-		return template.aggregate(aggregation, "conteggio", Object.class);
+		return template.aggregate(aggregation, "conteggio", Object.class).getRawResults();
 	}
 	
-	public AggregationResults<Object> listaDoppiaFiltra (String sigla) {
+	public Document listaDoppiaFiltra (String sigla) {
 		Aggregation aggregation = Aggregation.newAggregation(
 				Aggregation.match(Criteria.where("sigla").is(sigla)),
 				Aggregation.sort(Sort.by(Direction.DESC, "dose2")));
-		return template.aggregate(aggregation, "conteggio", Object.class);
+		return template.aggregate(aggregation, "conteggio", Object.class).getRawResults();
 	}
 }
