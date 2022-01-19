92af030498a12e234871466c00b402d5ee77898e
Alessandro <depasquale.a@tuta.io>
2022-01-19 14:08:25 +0100

    righe vuote

--- taskScheduler/src/main/java/com/depa/progettinocovid/taskScheduler/rest/SchedulerRestController.java
+++ taskScheduler/src/main/java/com/depa/progettinocovid/taskScheduler/rest/SchedulerRestController.java
@@ -1,31 +1,18 @@
 package com.depa.progettinocovid.taskScheduler.rest;
 
-
-import org.quartz.CronTrigger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.depa.progettinocovid.taskScheduler.service.SchedulerService;
-
 @RestController
 public class SchedulerRestController {
 	
-	@Autowired
-	private SchedulerService service;
-	
 	// intervallo numero di giorni? ore? boh
 	@GetMapping(path = "schedule/{tema}")
-	public ResponseEntity<String> schedule(@PathVariable String tema, @RequestParam String cron) {
-		CronTriggerFactoryBean triggerFactoryBean = new CronTriggerFactoryBean();
-		triggerFactoryBean.setCronExpression(cron);
-		CronTrigger trigger = triggerFactoryBean.getObject();
-		service.scheduleRichiesta(tema, trigger);
+	public ResponseEntity<String> schedule(@PathVariable String tema, @RequestParam int intervallo) {
 		return new ResponseEntity<String>("success", HttpStatus.OK);
 	}
 }
