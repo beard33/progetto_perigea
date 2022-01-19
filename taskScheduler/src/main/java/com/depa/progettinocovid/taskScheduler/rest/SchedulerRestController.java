6a87e7f9f340afb9a530f0a2204fc6e9a6927389
Alessandro <depasquale.a@tuta.io>
2022-01-19 09:46:46 +0100

    task scheduler

---
+++ taskScheduler/src/main/java/com/depa/progettinocovid/taskScheduler/rest/SchedulerRestController.java
@@ -0,0 +1,20 @@
+package com.depa.progettinocovid.taskScheduler.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SchedulerRestController {
+	
+	
+	// intervallo numero di giorni? ore? boh
+	@GetMapping(path = "schedule/{tema}")
+	public ResponseEntity<String> schedule(@PathVariable String tema, @RequestParam int intervallo) {
+		
+		return new ResponseEntity<String>("success", HttpStatus.OK);
+	}
+}
